c05534e57d923ee14219f6d1687546cca0de0310
hella <hellaim@yandex.ru>
2025-03-19 18:20:19 +0700

    regular fix

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Магазин',
+  description: 'Пример каталога с корзиной',
+}
+
 export default function index() {
   return <h1>Hello index</h1>
 }
